c01fbade386b38a9f4a6113a5623407647ce7716
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-04-17 13:55:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = 'Winner: ' + winner;
+    status = 'Vencedor: ' + winner;
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
